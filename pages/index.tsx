6994a095f1afdb1d556c758266db0a38a96d5b5c
Equilibris <williambilly42@gmail.com>
2020-05-12 19:56:47 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,10 @@
 import React from 'react'
 
-import { motion, useTransform, useViewportScroll } from 'framer-motion'
+// import { motion, useTransform, useViewportScroll } from 'framer-motion'
 
 import Style from '../client/style/landing.module.scss'
 
-function PathContainer({ style }: { style: React.CSSProperties }) {
-	return (
-		<g xmlns="http://www.w3.org/2000/svg" clip-path="url(#clip0)">
-			<path
-				d="M0 1024L527 497"
-				stroke="black"
-				stroke-width="60"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			/>
-			<path
-				d="M286 1024L637 673"
-				stroke="black"
-				stroke-width="11"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			/>
-			<path
-				d="M369 1024L720 673"
-				stroke="black"
-				stroke-width="11"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			/>
-			<path
-				d="M194 1024L545 673"
-				stroke="black"
-				stroke-width="11"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-			/>
-		</g>
-	)
-}
+
 
 export default function index() {
 	return (
