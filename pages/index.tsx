7c5898e4a4ada7f2e40475cf5748d5ea684c2303
Equilibris <williambilly42@gmail.com>
2020-05-14 22:23:19 +0200

    codebase refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,96 +73,123 @@ const Positioner: React.FC<{
 	x: number
 	y: number
 	height: number
-}> = ({ children, x, y, distance, height }) => {
+	start?: number
+}> = ({ children, x, y, distance, height, start }) => {
 	const { scrollY } = useViewportScroll()
-	const transform = useTransform(scrollY, [0, height], [0, 1], {
-		ease: (pre) => pre * distance + y,
-	})
+	const transform = useTransform(
+		scrollY,
+		[start || 0, height + (start || 0)],
+		[0, 1],
+		{
+			ease: (pre) => pre * distance + y,
+		}
+	)
 
 	return <motion.div style={{ x: x, y: transform }}>{children}</motion.div>
 }
 
-export default function index() {
-	const { vh: height, vw: width } = useViewport()
-
+function Header({ height, width }: { height: number; width: number }) {
 	return (
-		<>
-			<div className={`${Style.header} center`}>
-				<h1>BPM</h1>
-				<sub className="header-sub">BEEF PACKAGE MANAGER</sub>
-				<a href="#about" className={Style.navIcon}>
-					arrow_back_ios
-				</a>
+		<div className={`${Style.header} center`}>
+			<h1>BPM</h1>
+			<sub className="header-sub">BEEF PACKAGE MANAGER</sub>
+			<a href="#about" className={Style.navIcon}>
+				arrow_back_ios
+			</a>
 
-				<div className={`${Style.underlay} center`}>
-					{range(15).map((index) => {
-						const randomBool = (bias?: number) =>
-							Math.round(Math.random() - (bias || 0))
+			<div className={`${Style.underlay} center`}>
+				{range(15).map((index) => {
+					const randomBool = (bias?: number) =>
+						Math.round(Math.random() - (bias || 0))
 
-						const anySignRandom = () => (Math.random() - 0.5) * 2
+					const anySignRandom = () => (Math.random() - 0.5) * 2
 
-						const columns = Math.round(Math.random() * 7) + 3
-						const rows = Math.round(Math.random() * 7) + 3
+					const columns = Math.round(Math.random() * 7) + 3
+					const rows = Math.round(Math.random() * 7) + 3
 
-						const colors = [1, 2, 3, 5]
-						const color =
-							colors[Math.round(Math.random() * colors.length)]
+					const colors = [1, 2, 3, 5]
+					const color =
+						colors[Math.round(Math.random() * colors.length)]
 
-						return (
-							<Positioner
-								height={height * 100}
-								distance={Math.random() * 400 + 400}
-								x={anySignRandom() * width * 50}
-								y={anySignRandom() * height * 50 - 80}
-								key={index}
-							>
-								{randomBool(-0.1) ? (
-									<CircleGrid
-										circles={columns * rows}
-										columns={columns}
-										radius={Math.random() * 5 + 3}
-										gap={Math.random() * 50 + 10}
-										color={`var(--palet-${color})`}
-										border={
-											randomBool() ? '0px' : '10000px'
-										}
-									/>
-								) : (
-									<Circle
-										radius={Math.random() * 50 + 10}
-										color={`var(--palet-${color})`}
-									/>
-								)}
-							</Positioner>
-						)
-					})}
-				</div>
+					return (
+						<Positioner
+							height={height * 100}
+							distance={Math.random() * 400 + 400}
+							x={anySignRandom() * width * 50}
+							y={anySignRandom() * height * 50 - 80}
+							key={index}
+						>
+							{randomBool(-0.1) ? (
+								<CircleGrid
+									circles={columns * rows}
+									columns={columns}
+									radius={Math.random() * 5 + 3}
+									gap={Math.random() * 50 + 10}
+									color={`var(--palet-${color})`}
+									border={randomBool() ? '0px' : '10000px'}
+								/>
+							) : (
+								<Circle
+									radius={Math.random() * 50 + 10}
+									color={`var(--palet-${color})`}
+								/>
+							)}
+						</Positioner>
+					)
+				})}
 			</div>
-			<div className={`${Style.section} ${Style.about}`} id="about">
+		</div>
+	)
+}
+
+function Features() {
+	return (
+		<div
+			className={`${Style.about} center-grid ${Style.section} ${Style.noPad}`}
+			id="about"
+		>
+			<div></div>
+			<div className="center">
 				<h1>About</h1>
 				<p>
 					BPM is a package manager for Beef programming language. It
 					comes packaged with a Package Index, CLI and GraphQL API.
 				</p>
-				<h2>Slogans:</h2>
-				<p>Bringing the Beef community together</p>
-				<p>Create and share!</p>
 			</div>
-			<div className={`${Style.section}`} id="faq">
-				<h1>FAQ</h1>
-				<div>
-					<h2>How do I register my package?</h2>
-					<p>
-						Login with GitHub, go to Manage Packages and hit the
-						plus sign.
-					</p>
-					<h2>How do I install a package?</h2>
-					<p>
-						<a>Download the BPM CLI</a> and read the <a>How To</a>{' '}
-						in our guide.
-					</p>
-				</div>
+			{/* <h2>Slogans:</h2>
+			<p>Bringing the Beef community together</p>
+			<p>Create and share!</p> */}
+		</div>
+	)
+}
+
+function FAQ() {
+	return (
+		<div className={`${Style.section}`} id="faq">
+			<h1>FAQ</h1>
+			<div>
+				<h2>How do I register my package?</h2>
+				<p>
+					Login with GitHub, go to Manage Packages and hit the plus
+					sign.
+				</p>
+				<h2>How do I install a package?</h2>
+				<p>
+					<a>Download the BPM CLI</a> and read the <a>How To</a> in
+					our guide.
+				</p>
 			</div>
+		</div>
+	)
+}
+
+export default function index() {
+	const { vh: height, vw: width } = useViewport()
+	return (
+		<>
+			<Header height={height} width={width} />
+			<Features />
+			<FAQ />
 		</>
 	)
 }
