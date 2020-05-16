85c13617b4fbb59eff8932531a58b1733ab2a76b
Equilibris <williambilly42@gmail.com>
2020-05-16 22:25:25 +0200

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,14 @@ import useViewport from '../client/hooks/useViewport'
 
 import Style from '../client/style/landing.module.scss'
 
+type bound = [[number, number], [number, number]]
+
+const _boundingBoxIntersect = (a: bound, b: bound) =>
+	a[1][0] - b[0][0] > 0 && a[1][1] - b[0][1]
+
+const boundingBoxIntersect = (a: bound, b: bound) =>
+	_boundingBoxIntersect(a, b) || _boundingBoxIntersect(b, a)
+
 const Circle: React.FC<{
 	radius: number
 	color: string
@@ -101,6 +109,11 @@ function Header({
 	width: number
 	isMobile: boolean
 }) {
+	// 308 wide 175 hight
+	const textBound: bound = [
+		[width * 50 - 104, (width * 85) / 2 + 175 / 2],
+		[width * 50 + 104, (width * 85) / 2 - 175 / 2],
+	]
 	return (
 		<div className={`${Style.header} center`}>
 			<h1>BPM</h1>
@@ -111,20 +124,53 @@ function Header({
 
 			<div className={`${Style.underlay} center`}>
 				{range(15).map((index) => {
-					const [
+					function reRoll() {
+						;[
+							columnRandom,
+							rowRandom,
+							colorRandom,
+							radiusRandom,
+							gapRandom,
+						] = useRandoms(5)
+						isGrid = randomBool(-0.1)
+					}
+
+					let [
 						columnRandom,
 						rowRandom,
 						colorRandom,
 						radiusRandom,
 						gapRandom,
 					] = useRandoms(5)
+					let isGrid = randomBool(-0.1)
+					const colors = [1, 2, 3, 5]
+					const numCol = Math.round(colorRandom * colors.length)
+
+					const x = anySignRandom() * width * 50
+					const y = anySignRandom() * height * 50 - 80
+					const radius = isGrid
+						? radiusRandom * 5 + 3
+						: radiusRandom * 50 + 10
 
 					const columns = Math.round(columnRandom * 7) + 3
 					const rows = Math.round(rowRandom * 7) + 3
 
-					const colors = [1, 2, 3, 5]
 					const color =
 						colors[Math.round(colorRandom * colors.length)]
+					const thisBound: bound = isGrid
+						? [
+								[x, y],
+								[x + radius * columns, y],
+						  ]
+						: [
+								[x, y],
+								[x + 2 * radius, y + 2 * radius],
+						  ]
+
+					while (
+						boundingBoxIntersect(thisBound, textBound)
+					)
+						reRoll()
 
 					return (
 						<Positioner
@@ -133,22 +179,22 @@ function Header({
 							distance={
 								(Math.random() * 800 + 400) * (isMobile ? 3 : 1)
 							}
-							x={anySignRandom() * width * 50}
-							y={anySignRandom() * height * 50 - 80}
+							x={x}
+							y={y}
 							key={index}
 						>
-							{randomBool(-0.1) ? (
+							{isGrid ? (
 								<CircleGrid
 									circles={columns * rows}
 									columns={columns}
-									radius={radiusRandom * 5 + 3}
+									radius={radius}
 									gap={gapRandom * 50 + 10}
 									color={`var(--palet-${color})`}
 									border={randomBool() ? '0px' : '10000px'}
 								/>
 							) : (
 								<Circle
-									radius={radiusRandom * 50 + 10}
+									radius={radius}
 									color={`var(--palet-${color})`}
 								/>
 							)}
@@ -174,7 +220,9 @@ function Features({
 	const easeInOutCubic = (x: number) =>
 		x < 0.5 ? 4 * x * x * x : 1 - (-2 * x + 2) ** 3 / 2
 
-	const animationRange = isMobile ? [height * 70, height * 100] : [height * 50, height * 90]
+	const animationRange = isMobile
+		? [height * 70, height * 100]
+		: [height * 50, height * 90]
 	const x = useTransform(scrollY, animationRange, [100, 0], {
 		ease: (x) => easeInOutCubic(x),
 	})
