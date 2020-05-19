82859f548075df5a697a223fb10aa808ae26ceee
Equilibris <williambilly42@gmail.com>
2020-05-19 12:01:52 +0200

    resolved missunderstanding with package placement

--- pages/package/[PID].tsx
+++ pages/package/[PID].tsx
@@ -6,15 +6,15 @@ export default function ReadID() {
 	return (
 		<>
 			<div className={`${Style.packageInfo}`}>
-				<h1>Steak.Logging</h1>
-				<p>By: RogueMacro</p>
+				<h1>TITLE</h1>
+				<p>By: AUTHOR</p>
 				<h3>Downloads</h3>
 				<hr />
 				<p>All: 55432</p>
 				<p>Monthly: 5423</p>
 				<p>Weekly: 831</p>
 				<p>Daily: 43</p>
-				<h3>Repository: https://github.com/RogueMacro/Steak.Logging</h3>
+				<h3>Repository: REPO</h3>
 			</div>
 			<div className={`${Style.readme}`}>
 				<h3>-- HERE GOES README --</h3>
