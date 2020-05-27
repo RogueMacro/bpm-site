1a6e5a241103a6ffec94c078bb08f3208342029b
Equilibris <williambilly42@gmail.com>
2020-05-27 15:41:39 +0200

    reverted to next server spec for ssr ssg support

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 import * as expressTypes from 'express'
 import { Project as ProjectArgs } from './global/typings/StaticProps'
 
+import _next from 'next'
 import { initializeApp as initializeFirebaseApp } from './node_modules/firebase-admin/lib'
 
 import { join } from 'path'
 import { parse } from 'url'
 
-import editNextDoc from './server/components/editNext.component'
 import gql from './api/server'
 import _generate from './server/components/generate.component'
-import getFilePathFromChain from './server/utils/getFilePathFromChain'
 
 const PORT = process.env.PORT || 8080
 
@@ -21,29 +20,13 @@ initializeFirebaseApp({
 
 const root = join(__dirname, 'out')
 
-const resolver = (path: string): Promise<string> =>
-	new Promise((resolve, reject) => {
-		path = join(root, path)
-
-		getFilePathFromChain(
-			[
-				path,
-				path + '.html',
-				path + '.htm',
-				path + 'index.html',
-				join(path, '/index.html'),
-				join(path, '/index.htm'),
-				root + '/404.html',
-			],
-			'file'
-		).then((v) => {
-			resolve(v)
-		}, reject)
-	})
-
-const generate = _generate(resolver)
 
 const express = require('express')
+
+const next = _next({
+	dev: (process.env.NODE_ENV || 'production') === 'development',
+})
+const handel = next.getRequestHandler()
 
 const app: expressTypes.Application = express()
 
@@ -58,50 +41,26 @@ app.get(/\/assets\//, (req, res) => {
 
 app.get(/favicon.ico/, (_, res) => res.redirect(303, '/assets/bpm_logo.svg'))
 
-app.get(/_next/, (req, res) =>
-	resolver(parse(req.url).pathname).then((fName) => {
-		res.sendFile(fName)
-		setTimeout(() => res.end(), 100)
+next.prepare().then(() => {
+	app.get(/(package|p)\/.*/s, (req, res) => {
+		const url = parse(req.url)
+		next.render(req,res,'/package/package')
 	})
-)
 
-app.get(/.*?\/(package|p)\/.*/, (req, res) => {
-	generate({
-		url: '/package/package',
-		userAgent: req.headers['user-agent'],
-	}).then(([doc]) => {
-		const resultingDoc = editNextDoc<ProjectArgs>(doc, {
-			props: {
-				pageProps: {
-					author: 'author',
-					downloads: { daily: 10, monthly: 10, total: 10, weekly: 10 },
-					readMe: 'test',
-					repo: 'test',
-					title: 'test',
-				},
-				__N_SSG: true,
-			},
+	app.get(/.*/s, (req, res) => {
+		handel(req, res).then(() => {
+			if (!req.url.includes('/_next'))
+				console.log(
+					`connection to ${req.url} from [ ${[req.ip, ...req.ips].join(' ')} ]`
+				)
 		})
-
-		res.send(resultingDoc)
 	})
-})
 
-app.get(/.*/, (req, res) => {
-	generate({ url: req.url, userAgent: req.headers['user-agent'] }).then(
-		([value]) => {
-			if (value) {
-				res.send(value)
-			}
-		},
-		console.error
-	)
-})
-
-app.listen(PORT, () => {
-	console.log(
-		'initialize',
-		`server listening on port ${PORT}`,
-		'local:networking'
-	)
+	app.listen(PORT, () => {
+		console.log(
+			'initialize',
+			`server listening on port ${PORT}`,
+			'local:networking'
+		)
+	})
 })
