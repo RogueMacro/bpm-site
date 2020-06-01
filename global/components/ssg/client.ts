77bfd3b8678cd1c2360fe4979a7dadd704b62666
Equilibris mobile <47296141+Equilibris@users.noreply.github.com>
2020-06-01 14:48:33 +0200

    fixed small bug

--- global/components/ssg/client.ts
+++ global/components/ssg/client.ts
@@ -3,5 +3,5 @@ import { GetServerSideProps } from 'next'
 export const getServerSideProps: GetServerSideProps = async ({
 	query: { payload, ...query },
 }) => {
-	return { props: { ...query, ...JSON.parse(`${payload || '{}'}`) } }
+	return { props: { query, ...JSON.parse(`${payload || '{}'}`) } }
 }
