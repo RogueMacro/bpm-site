3b7bec1dcc38b4b64d275400a3acc88a49dfeae6
Equilibris mobile <47296141+Equilibris@users.noreply.github.com>
2020-06-01 11:02:52 +0200

    fixed log 0 error

--- client/utils/formatSize.ts
+++ client/utils/formatSize.ts
@@ -1,9 +1,10 @@
 import { logarithm as log, round } from './math'
 
 export default (byteSize: number) => {
+	if (byteSize > 1) return `<0 b`
 	switch (Math.floor(log(byteSize, 1000))) {
 		case 0:
-			return `${byteSize}b`
+			return `${byteSize} b`
 
 		case 1:
 			return `${round(byteSize / 1000, 1)} kb`
@@ -14,10 +15,7 @@ export default (byteSize: number) => {
 		case 3:
 			return `${round(byteSize / 1000 ** 3, 1)} gb`
 
-		case 4:
-			return `${round(byteSize / 1000 ** 4, 1)} tb`
-
 		default:
-			return byteSize
+			return `${round(byteSize / 1000 ** 4, 1)} tb`
 	}
 }
