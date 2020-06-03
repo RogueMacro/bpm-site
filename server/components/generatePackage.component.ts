c4a23aee076507508209d03dde6e7f7c6254fab2
Equilibris <williambilly42@gmail.com>
2020-06-03 17:43:17 +0200

    implemented readme fetching

--- server/components/generatePackage.component.ts
+++ server/components/generatePackage.component.ts
@@ -1,8 +1,24 @@
 import Context from '../typings/context.type'
-
-// import axios from 'axios'
-
 import * as staticProps from '../../global/typings/props'
+
+import { cacheForAsync } from '../utils/cache'
+import { parse } from 'url'
+import axios from 'axios'
+
+const getReadme = cacheForAsync(
+	(repo: string) =>
+		new Promise((resolve, reject) => {
+			axios
+				.get(
+					`https://raw.githubusercontent.com/${repo}/master/README.md`
+				)
+				.then((res) => {
+					console.log(res.data)
+					resolve(res.data)
+				}, reject)
+		}),
+	1000 * 60 * 60 * 12
+)
 
 const getPackage = ({ packages }: Context) => (packageID: string) =>
 	new Promise((resolve, reject) =>
@@ -16,9 +32,18 @@ const getPackage = ({ packages }: Context) => (packageID: string) =>
 						staticProps.Project
 					>
 					if (data && data.author && data.name && data.repo) {
-                        // axios.get('')
+						const repo = parse(data.repo).pathname
 
-						resolve({ title: data.name, ...data })
+						console.log(repo)
+						if (repo) {
+							getReadme(repo).then((str) =>
+								resolve({
+									title: data.name,
+									readMe: str,
+									...data,
+								})
+							)
+						} else resolve({ title: data.name, ...data })
 					} else resolve(null)
 				}
 			}, reject)
